b296fa7cc68678309d58da5d49c31f02eb18cc23
Roger Bosch Mateo <boschmateo@MacBook-Pro-de-Roger.local>
2020-04-08 09:23:41 +0200

    Flutter inner files modified

--- lib/screens/interfaceScreen.dart
+++ lib/screens/interfaceScreen.dart
@@ -1,10 +1,23 @@
-
-
+import 'package:betogether/models/user.dart';
+import 'package:betogether/services/cognito_service.dart';
+import 'package:amazon_cognito_identity_dart_2/src/cognito_user_pool.dart';
 import 'package:flutter/material.dart';
 import 'addScreen.dart';
 import 'homeScreen.dart';
 import 'listScreen.dart';
-import 'profileScreen.dart';
+
+//TODO: This are variables used for the authentication endpoint.
+// It is porbably best to refactor this somewhere else. In the meantime
+// it stays here.
+
+// Setup AWS User Pool Id & Client Id settings here:
+const _awsUserPoolId = 'eu-west-2_G1chdG3tq';
+const _awsClientId = '19j1sl25biq0uu4opvp0tv7nkp';
+
+// Setup endpoints here:
+const _region = 'eu-west-2';
+const _endpoint =
+    'https://9gzf9ud1r6.execute-api.eu-west-2.amazonaws.com/dev';
 
 class InterfacePage extends StatefulWidget {
   InterfacePage({Key key, this.title}) : super(key: key);
@@ -15,6 +28,20 @@ class InterfacePage extends StatefulWidget {
 }
 
 class _InterfacePageState extends State<InterfacePage> {
+
+  // User variables to share amongst all the screens
+  UserService _userService;
+  User _user;
+  bool _isAuthenticated = false;
+
+  @override
+  void initState() {
+    final userPool = new CognitoUserPool(_awsUserPoolId, _awsClientId);
+    _userService = new UserService(userPool);
+    _user = new User();
+    _userService.init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +61,7 @@ class _InterfacePageState extends State<InterfacePage> {
               new HomeScreen(),
               new ListScreen(),
               new AddScreen(),
-              new ProfileScreen()
+              open_profile_screen(context)
             ],
           ),
           bottomNavigationBar: new TabBar(
@@ -63,4 +90,38 @@ class _InterfacePageState extends State<InterfacePage> {
       ),
     );
   }
+
+
+  Widget open_profile_screen(BuildContext context){
+    return new FutureBuilder(
+      future: _getValues(),
+      builder: (context, AsyncSnapshot<UserService> snapshot) {
+        if (snapshot.hasData) {
+          if (_isAuthenticated){
+            return Text('View Profile');
+          }
+          else if(_user == null){
+            return new Text('Signup/Login Scren');
+          }
+          else if(!_user.confirmed){
+            return new Text("Confirm account");
+          }
+          else{
+            return new Text("Shouldn't be here");
+          }
+
+        }
+        return Center(
+          child: new CircularProgressIndicator(),
+        );
+      }
+      );
+  }
+
+  Future<UserService> _getValues() async {
+      await _userService.init();
+      _isAuthenticated = await _userService.checkAuthenticated();
+      _user = await _userService.getCurrentUser();
+      return _userService;
+    }
 }
