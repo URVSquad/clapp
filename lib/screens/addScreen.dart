3dcc76f7d8da1a6627190f10e5b9075fb8e54791
Javier Ortega <javier.ortega@estudiants.urv.cat>
2020-04-09 11:53:10 +0200

    Botones y cambio de vista

--- lib/screens/addScreen.dart
+++ lib/screens/addScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
 
 class AddScreen extends StatefulWidget {
   AddScreen() : super();
@@ -12,8 +14,55 @@ class _AddScreenState extends State<AddScreen> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      //Here
+        body: Center(
+          child: Column( children: <Widget>[
+              RaisedButton(
+                onPressed: _create,
+                child: Text('Button', style: TextStyle(fontSize: 20)),
+              ),
+              RaisedButton(
+                onPressed: _create,
+                child: Text('Button', style: TextStyle(fontSize: 20)),
+              ),
+            ])
+        )
+    );
+  }
 
+  void _create() {
+    final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  FormBuilder(
+                    key: _fbKey,
+                    initialValue: {
+                      'date': DateTime.now(),
+                      'accept_terms': false,
+                    },
+                    autovalidate: true,
+                    child: Column(
+                      children: <Widget>[
+                        FormBuilderDateTimePicker(
+                          attribute: "date",
+                          inputType: InputType.date,
+                          format: DateFormat("yyyy-MM-dd"),
+                          decoration:
+                          InputDecoration(labelText: "Appointment Time"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      )
     );
   }
 }
